235b762c67976830f8d413a5d3bc7f7e3387d978
Sarah Shields <sshield9@my.centennialcollege.ca>
2025-03-26 21:21:38 -0400

    feat: integrate Header and Footer layout components
    refs Task 41
    part of User Story 39

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -7,11 +7,14 @@ import ListStudents from './pages/ListStudents';
 import ListCourses from './pages/ListCourses';
 import CreateCourse from './pages/CreateCourse';
 import RegisterPage from './pages/RegisterPage';
+import Header from './pages/Header';
+import Footer from './pages/Footer';
 
 function App() {
     return (
       <UserProvider>
         <Router>
+            <Header />
             <Routes>
                 <Route path="/" element={<LoginPage />} />
                 <Route path="/register" element={<RegisterPage />} />
@@ -21,6 +24,7 @@ function App() {
                 <Route path="/list-students" element={<ListStudents />} />
                 <Route path="/list-courses" element={<ListCourses />} />
             </Routes>
+            <Footer />
         </Router>
       </UserProvider>
     );
